63d103b0eb36cfbc7e85bbb2abbab8a6459b56e6
Jon Belaire <jon.belaire@icloud.com>
2021-02-27 10:27:01 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pomodoro Timer
+An application that automatically oscillates between focus time and break time.
+## Features
+### Focus Time Setting
+Set the amount of time that you want to focus.
+### Break Time Setting
+Set the amount of time that you want to take a break.
+### Progress Bar
+Enjoy a visual representation of the progress of your current session.
+### Duration
+Also enjoy a visual representation of the remaining time your current session has.
---
+++ build/precache-manifest.41bade4e3d7819ab9dc5ba7dd1d31309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb156a76f84d8c59e69c2ee4a4aa937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3cbc247d48a1183860",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "51094f3fe8fe0955c394",
+    "url": "/static/js/2.51445301.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.51445301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3cbc247d48a1183860",
+    "url": "/static/js/main.ea1bff7a.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
